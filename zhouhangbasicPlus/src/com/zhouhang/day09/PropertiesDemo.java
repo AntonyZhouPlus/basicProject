8fb20e943186e42f4a62543cd247ebfbc2ecac21
AntonyZhouPlus <376083839@qq.com>
2018-06-11 11:29:45 +0800

    June 10rd

---
+++ zhouhangbasicPlus/src/com/zhouhang/day09/PropertiesDemo.java
@@ -0,0 +1,45 @@
+package com.zhouhang.day09;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Properties;
+
+/**
+ * com.zhouhang.day09
+ *
+ * @author zhouhang
+ * @date 2018/6/11 上午10:08
+ */
+public class PropertiesDemo {
+    public static void main(String[] args) throws IOException {
+        Properties p = new Properties();
+        FileReader fr = new FileReader("store.txt");
+        p.load(fr);
+        fr.close();
+        for (String s : p.stringPropertyNames()) {
+            System.out.println(s + "===" + p.getProperty(s));
+        }
+    }
+
+    private static void store() throws IOException {
+        Properties p = new Properties();
+        p.setProperty("A","1");
+        p.setProperty("B","2");
+        p.setProperty("C","3");
+        FileWriter fw = new FileWriter("store.txt");
+        p.store(fw,"test");
+        fw.close();
+    }
+
+    private static void print() {
+        Properties p = new Properties();
+        p.setProperty("A","1");
+        p.setProperty("B","2");
+        p.setProperty("C","3");
+        for (String s : p.stringPropertyNames()) {
+            System.out.println(s + "===" + p.getProperty(s));
+        }
+    }
+}
