7658fc5b25727a4b5c532a2620cc2aef0fecb2c9
AntonyZhouPlus <376083839@qq.com>
2018-06-15 10:08:58 +0800

    June 15 day12homework final version

--- zhouhangbasicPlus/src/com/zhouhang/day12homework/test10/Test10.java
+++ zhouhangbasicPlus/src/com/zhouhang/day12homework/test10/Test10.java
@@ -2,6 +2,7 @@ package com.zhouhang.day12homework.test10;
 
 
 import java.util.HashMap;
+import java.util.Map;
 import java.util.function.Consumer;
 
 /**
@@ -16,12 +17,32 @@ import java.util.function.Consumer;
  */
 public class Test10 {
     public static void main(String[] args) {
-        HashMap<String, Integer> map = new HashMap<>();
-        map.put("岑小村", 59);
-        map.put("谷天洛", 82);
-        map.put("渣渣辉", 98);
-        map.put("蓝小月", 65);
-        map.put("皮几万", 70);
+        HashMap<String, Integer> hashMap = new HashMap<>();
+        hashMap.put("岑小村", 59);
+        hashMap.put("谷天洛", 82);
+        hashMap.put("渣渣辉", 98);
+        hashMap.put("蓝小月", 65);
+        hashMap.put("皮几万", 70);
+
+        doJob(hashMap,
+                (map)->{
+                    System.out.println("青铜段位以上:");
+                    for (Map.Entry<String, Integer> entry : map.entrySet()) {
+                        if (entry.getValue() >= 60) {
+                            System.out.println(entry.getKey());
+                        }
+                    }
+                },
+                (map)->{
+                    System.out.println("=================");
+                    System.out.println("我是王者：");
+                    for (Map.Entry<String, Integer> entry : map.entrySet()) {
+                        if (entry.getValue() >= 70) {
+                            System.out.println(entry.getKey());
+                        }
+                    }
+
+                });
     }
 
     public static <T> void doJob(T t, Consumer<T> consumer1, Consumer<T> consumer2) {
